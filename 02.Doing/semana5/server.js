8cc4ca2b49f080405193f98292e9085915263a20
oscaresp96 <orlandoseas18@gmail.com>
2025-06-26 21:36:22 -0600

    updates

--- 02.Doing/semana5/server.js
+++ 02.Doing/semana5/server.js
@@ -18,10 +18,67 @@ app.get('/:name', logger, (req, res) => {
   else {
     res.end(`Welcome ${req.params.name}`);
   }
-  res.end('Hola');
 });
 
 app.use('/api', usersRouter);
+
+app.get('/saludo/:name',logger, (req, res) => {
+  const { name } = req.params;
+  res.json({message: `Hola ${name}`});
+});
+
+app.get('/api/edad', logger, (req, res) => {
+  const anioNacimiento = parseInt(req.query.anioNacimiento);
+  const actual = new Date().getFullYear();
+
+  if(!anioNacimiento || anioNacimiento > actual) {
+    console.log(anioNacimiento, actual);
+    return res.status(400).json({error: 'Año inválido'});
+  }
+
+  const edad = actual - anioNacimiento;
+  res.json({anioNacimiento, edad});
+});
+
+app.get('/suma/:a/:b', logger, (req, res) => {
+  const { a, b } = req.params;
+  const suma = Number(a) + Number(b);
+
+if (isNaN(suma)) {
+    return res.status(400).json({error: 'Parámetros inválidos'});
+  }
+
+  res.json ({suma});
+});
+
+app.get('/perfil/:usuario', logger, (req, res) => {
+  const { usuario } = req.params;
+  const {lang} = req.query;
+  let mensaje=`Welcome ${usuario}`;
+  if (lang === 'es') {
+    mensaje = `Bienvenido ${usuario}`;
+  } else if (lang === 'en') {
+    mensaje = `Welcome ${usuario}`;
+  } else if (lang === 'fr') {
+    mensaje = `Bienvenue ${usuario}`;
+  } else if (lang === 'de') {
+    mensaje = `Willkommen ${usuario}`;
+  }
+
+  res.json({mensaje,language:lang || 'default'});
+});
+
+app.get('/api/buscar', logger, (req, res) => {
+  const {producto, categoria} = req.query;
+  if (!producto || !categoria) {
+    return res.status(400).json({mensaje: 'Faltan parámetros en la ruta'});
+  }
+res.json({
+  busqueda:producto,
+  categoria,
+  mensaje: `Buscando ${producto} en la categoría ${categoria}`
+});
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en http://localhost:${PORT}`);
